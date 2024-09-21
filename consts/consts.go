2b5506d7eced412ca1c46ddca0cef05c5e26a307
samuelsunmisola119 <samuelsunmisola119@gmail.com>
2024-09-21 09:18:40 +0100

    initial commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "SUNMISOLA"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "SUNMI"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "S"
 )
 
 var ID ids.ID
